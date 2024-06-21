d152d9b1491632dc4be7d546a0dd8dba6a8752da
Bruno Matheus Ribeiro <brunomatheus168@gmail.com>
2024-06-21 17:33:17 -0300

    "FindById implementation"

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,17 @@
 package application;
 
-import entities.Department;
 import entities.Seller;
-import java.util.Date;
 import modelDao.DaoFactory;
 import modelDao.SellerDao;
 
 public class Program {
     public static void main(String[] args) throws Exception {
        
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "BOB", "bob@gmail.com", new Date(), 3000.0,obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+        
         System.out.println(seller);
     }
 }
