65b75aeff9772f3ef62e0c3a3f6732869d1266b4
Bruno Matheus Ribeiro <brunomatheus168@gmail.com>
2024-06-27 15:20:27 -0300

    "DepartmentDaoJDBC implementation" - FINISHED

--- src/modelDao/DaoFactory.java
+++ src/modelDao/DaoFactory.java
@@ -1,6 +1,7 @@
 package modelDao;
 
 import db.DB;
+import modeldaoimpl.DepartmentDaoJDBC;
 import modeldaoimpl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -8,4 +9,8 @@ public class DaoFactory {
     public static SellerDao createSellerDao(){
         return  new SellerDaoJDBC(DB.getConnection());
         } 
+
+    public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
